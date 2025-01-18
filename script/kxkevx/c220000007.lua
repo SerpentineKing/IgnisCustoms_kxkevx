248a6203d6daa3811d6ba4c55f48d1178dc240a8
SKX <stormraida01@gmail.com>
2025-01-17 22:23:50 -0500

    test 0.0.1.0.i

--- script/kxkevx/c220000007.lua
+++ script/kxkevx/c220000007.lua
@@ -3,14 +3,40 @@ local s,id,o=GetID()
 -- c220000007
 function s.initial_effect(c)
 	-- [Activation]
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_ACTIVATE)
-	e0:SetCode(EVENT_FREE_CHAIN)
-	c:RegisterEffect(e0)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.e1tgt)
+	c:RegisterEffect(e1)
 	-- Neither player can activate cards or effects in response to this card’s activation.
 	--[[
 	Select 2 Monster Zones on the field.
 	Neither player can use the selected zones (even if this card leaves the field).
 	]]--
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_DISABLE_FIELD)
+	e2:SetOperation(s.e2evt)
+	e2:SetLabelObject(e1)
+	Duel.RegisterEffect(e2,tp)
 end
 -- Helpers
+function s.e1tgt(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local z1=Duel.GetLocationCount(tp,LOCATION_MZONE,PLAYER_NONE,0)
+		local z2=Duel.GetLocationCount(1-tp,LOCATION_MZONE,PLAYER_NONE,0)
+		return (z1+z2)>1
+	end
+
+	local z=Duel.SelectDisableField(tp,2,LOCATION_MZONE,LOCATION_MZONE,0)
+	
+	Duel.Hint(HINT_ZONE,tp,z)
+	e:SetLabel(z)
+
+	if e:IsHasType(EFFECT_TYPE_ACTIVATE) then
+		Duel.SetChainLimit(aux.FALSE)
+	end
+end
+function s.e2evt(e,tp)
+	return e:GetLabelObject():GetLabel()
+end
