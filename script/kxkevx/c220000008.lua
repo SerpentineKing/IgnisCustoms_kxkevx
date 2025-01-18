b534842a7a67854f65b7d8af7e9e4fb87dfeb5ad
SKX <stormraida01@gmail.com>
2025-01-18 11:26:50 -0500

    test 0.0.1.1.h [f]

--- script/kxkevx/c220000008.lua
+++ script/kxkevx/c220000008.lua
@@ -72,15 +72,16 @@ function s.e0tgt(e,tp,eg,ep,ev,re,r,rp,chk)
 		Duel.SetChainLimit(aux.FALSE)
 	end
 end
-function s.e1con(e,tp)
-	return e:GetHandler():GetReasonPlayer()==1-tp
+function s.e1con(e)
+	local c=e:GetHandler()
+	return c:GetReasonPlayer()~=c:GetOwner()
 end
 function s.e1tgt(e,c)
 	return c~=e:GetHandler()
 end
-function s.e3con(e)
+function s.e3con(e,tp)
 	local c=e:GetHandler()
-	return c:IsReason(REASON_EFFECT) and c:GetReasonPlayer()~=c:GetOwner()
+	return c:IsReason(REASON_EFFECT) and c:GetReasonPlayer()~=1-tp
 end
 function s.e3tgt(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
