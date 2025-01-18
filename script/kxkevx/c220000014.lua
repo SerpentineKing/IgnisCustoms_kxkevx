f04f862ffd6a9e169ece54b282d4e14fe5ea044f
SKX <stormraida01@gmail.com>
2025-01-18 17:36:06 -0500

    test 0.0.2.0.a [f]

--- script/kxkevx/c220000014.lua
+++ script/kxkevx/c220000014.lua
@@ -4,7 +4,7 @@ local s,id,o=GetID()
 function s.initial_effect(c)
 	-- Neither player can activate cards or effects in response to this card’s activation.
 	--[[
-	If you take battle or effect damage, OR if “King of Kings, Lord of Lords” in your GY:
+	If you take battle or effect damage, OR if “King of Kings, Lord of Lords” in your GY (Quick Effect):
 	You can discard this card; this card gains the following effects while in the GY.
 	•
 	You take no battle or effect damage.
@@ -22,14 +22,14 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1a)
 	
 	local e1b=Effect.CreateEffect(c)
-	e1b:SetDescription(aux.Stringid(id,1))
+	e1b:SetDescription(aux.Stringid(id,0))
 	e1b:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e1b:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL+EFFECT_FLAG_DELAY)
 	e1b:SetRange(LOCATION_HAND)
 	e1b:SetCode(EVENT_DAMAGE)
 	e1b:SetCost(s.e1cst)
 	e1b:SetCondition(s.e1bcon)
-	e1b:SetTarget(s.e1tgt)
+	e1b:SetTarget(s.e1btgt)
 	e1b:SetOperation(s.e1evt)
 	c:RegisterEffect(e1b)
 end
@@ -92,14 +92,21 @@ function s.e1evt(e,tp)
 	e1d2:SetCode(EFFECT_CANNOT_LOSE_EFFECT)
 	c:RegisterEffect(e1d2)
 end
+function s.e1bcon(e,tp,eg,ep,ev,re,r)
+	return ep==tp
+	and r&(REASON_BATTLE+REASON_EFFECT)~=0
+end
+function s.e1btgt(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+
+	if e:IsHasType(EFFECT_TYPE_QUICK_O) or e:IsHasType(EFFECT_TYPE_FIELD) then
+		Duel.SetChainLimit(aux.FALSE)
+	end
+end
 function s.e1cval(e,re,val,r)
 	if (r&REASON_EFFECT)~=0 then
 		return 0
 	else
 		return val
 	end
-end
-function s.e1bcon(e,tp,eg,ep,ev,re,r)
-	return ep==tp
-	and r&(REASON_BATTLE+REASON_EFFECT)~=0
 end
