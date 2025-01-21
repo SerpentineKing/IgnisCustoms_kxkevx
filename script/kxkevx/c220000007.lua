5cb19772ec1f8cdec2da729e07fb16adad0a513a
SKX <stormraida01@gmail.com>
2025-01-20 22:19:30 -0500

    test 0.0.2.1.b [f2]

--- script/kxkevx/c220000007.lua
+++ script/kxkevx/c220000007.lua
@@ -18,12 +18,12 @@ end
 -- Helpers
 function s.e1tgt(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
-		local z1=Duel.GetLocationCount(tp,LOCATION_MZONE+LOCATION_EMZONE,PLAYER_NONE,0)
-		local z2=Duel.GetLocationCount(1-tp,LOCATION_MZONE+LOCATION_EMZONE,PLAYER_NONE,0)
+		local z1=Duel.GetLocationCount(tp,LOCATION_MZONE)
+		local z2=Duel.GetLocationCount(1-tp,LOCATION_MZONE)
 		return (z1+z2)>1
 	end
 
-	local z=Duel.SelectDisableField(tp,2,LOCATION_MZONE+LOCATION_EMZONE,LOCATION_MZONE+LOCATION_EMZONE,0)
+	local z=Duel.SelectDisableField(tp,2,LOCATION_MZONE,LOCATION_MZONE,0,true)
 	Duel.Hint(HINT_ZONE,tp,z)
 	e:SetLabel(z)
 
